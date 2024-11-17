d8110d2e4570ec29713f868af4d986ec2c59a7e8
Lucas Nunes <161032233+lmnunes2002@users.noreply.github.com>
2024-11-17 23:41:51 +0000

    feature-continua o crud(main não incializa)

--- app/main.py
+++ app/main.py
@@ -23,38 +23,36 @@ def main():
 
         # Solicitando dados do usuário.
         if escolha == 1:
-            try:
-                print("\n--- Adicionar Usuário ---")
-                nome = input("Digite o nome do usuário: ").strip()
-                email = input("Digite o email do usuário: ").strip()
-                senha = input("Digite a senha do usuário: ").strip()
+            print("\n--- Adicionar Usuário ---")
+            nome = input("Digite o nome do usuário: ").strip()
+            email = input("Digite o email do usuário: ").strip()
+            senha = input("Digite a senha do usuário: ").strip()
 
-                # Verificar se os campos obrigatórios estão preenchidos
-                if not nome or not email or not senha:
-                    print("Erro: Todos os campos são obrigatórios!")
-                    return
+            # Verificar se os campos obrigatórios estão preenchidos
+            if not nome or not email or not senha:
+                print("Erro: Todos os campos são obrigatórios!")
+                return
 
-                # Chamar o serviço para criar o usuário
-                service.criar_usuario(nome, email, senha)
-            except ValueError as ve:
-                print(f"Erro de validação: {ve}")
-            except TypeError as te:
-                print(f"Erro de tipo: {te}")
-            except Exception as e:
-                print(f"Erro inesperado: {e}")
+            # Chamar o serviço para criar o usuário
+            service.criar_usuario(nome, email, senha)
         
-    # # Busca usuário por email.
-    # elif escolha == "2":
-    #         email = input("Digite o email do usuário para busca: ")
-    #         service.buscar_usuario_por_email(email)
+        # Busca usuário por email.
+        elif escolha == "2":
+                email = input("Digite o email do usuário para busca: ")
+                service.buscar_usuario_por_email(email)
 
-    # # Exibindo dados dos usuários.
-    # elif escolha == 7:
-    #     print("\nListando usuários cadastrados: ")
-    #     lista_usuarios = service.listar_todos_usuarios()
+        # Busca usuário por email.
+        elif escolha == "3":
+                email = input("Digite o id do usuário para busca: ")
+                service.buscar_usuario_por_id(id)
 
-    #     for usuario in lista_usuarios:
-    #         print(f"\nNome: {usuario.id} \nE-mail: {usuario.email} \nSenha: {usuario.senha}\n")
+        # Exibindo dados dos usuários.
+        elif escolha == 7:
+            print("\nListando usuários cadastrados: ")
+            lista_usuarios = service.listar_todos_usuarios()
+
+            for usuario in lista_usuarios:
+                print(f"\nNome: {usuario.id} \nE-mail: {usuario.email} \nSenha: {usuario.senha}\n")
 
 if __name__ == "__main__":
     os.system("cls||clear")
